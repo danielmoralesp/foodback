6b3f41140f6a6ef4d009e9a8f3d61ca259ab831d
danielmoralesp <daniel_moralesp@hotmail.com>
2017-03-13 16:43:58 -0500

    Pages and Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'restaurants#index'
 
   resources :restaurants
+
+  match '/about_us', to: 'pages#about_us', via: :get
+  match '/contact', to: 'pages#contact', via: :get
 
 end
