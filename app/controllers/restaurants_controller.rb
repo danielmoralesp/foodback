a30e651d7c55f287a24c1aeef662587ca0088a1a
danielmoralesp <daniel_moralesp@hotmail.com>
2017-03-13 17:54:08 -0500

    Create restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,5 +2,28 @@ class RestaurantsController < ApplicationController
 
   def index
   end
-  
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to @restaurant
+    else
+      flash[:error] = @restaurant.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :description, :address1, :address2, :city, :state, :zipcode, :phone, :email)
+    end
+
 end
